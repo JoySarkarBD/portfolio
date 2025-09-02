37070f74054013bbab0bfc93e0bac9def4fe2d08
JoySarkarBD <developer.joysarkar@gmail.com>
2025-09-02 13:28:19 +0600

    Add new favicon image (ms-icon-70x70.png) to public directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type { Metadata } from "next";
 import { Madimi_One } from "next/font/google";
 import { ThemeProvider } from "./_components/theme-provider";
@@ -9,10 +8,23 @@ const madimi_one = Madimi_One({
   subsets: ["latin"],
 });
 
-
 export const metadata: Metadata = {
   title: "Portfolio - Joy Sarkar",
   description: "Showcasing the work of Joy Sarkar",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", type: "image/x-icon" },
+      { url: "/android-icon-192x192.png", type: "image/png", sizes: "192x192" },
+      { url: "/android-icon-144x144.png", type: "image/png", sizes: "144x144" },
+      { url: "/android-icon-96x96.png", type: "image/png", sizes: "96x96" },
+      { url: "/android-icon-72x72.png", type: "image/png", sizes: "72x72" },
+      { url: "/android-icon-48x48.png", type: "image/png", sizes: "48x48" },
+      { url: "/android-icon-36x36.png", type: "image/png", sizes: "36x36" },
+    ],
+    apple: [{ url: "/apple-icon.png", sizes: "180x180", type: "image/png" }],
+    shortcut: [{ url: "/favicon.ico", type: "image/x-icon" }],
+  },
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
@@ -25,11 +37,10 @@ export default function RootLayout({
       <body
         className={`${madimi_one.variable} antialiased leading-8 overflow-x-hidden`}>
         <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
+          attribute='class'
+          defaultTheme='system'
           enableSystem
-          disableTransitionOnChange
-        >
+          disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>

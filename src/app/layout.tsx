cd461b1e2d264af795e18be10ab76269dfbec53d
diptowebhero <diptodas1672@gmail.com>
2025-09-02 12:54:22 +0600

    update logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 
 import type { Metadata } from "next";
-import { Madimi_One, Outfit, Ovo } from "next/font/google";
+import { Madimi_One } from "next/font/google";
 import { ThemeProvider } from "./_components/theme-provider";
 import "./globals.css";
 const madimi_one = Madimi_One({
@@ -8,15 +8,7 @@ const madimi_one = Madimi_One({
   variable: "--font-medmi",
   subsets: ["latin"],
 });
-const outfit = Outfit({
-  weight: ["400", "500", "600", "700"],
-  subsets: ["latin"],
-});
 
-const ovo = Ovo({
-  weight: ["400"],
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Portfolio - Joy Sarkar",
@@ -31,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang='en' className='scroll-smooth' suppressHydrationWarning>
       <body
-        className={`${outfit.className} ${ovo.className} ${madimi_one.variable} antialiased leading-8 overflow-x-hidden`}>
+        className={`${madimi_one.variable} antialiased leading-8 overflow-x-hidden`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
